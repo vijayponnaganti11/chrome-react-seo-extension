ef224715a295af342fed089b1ab012a791bd23b5
vijay shekhar <vijay.shekhar463@gmai.com>
2023-01-15 12:11:12 +0530

    Chrome tabs eventListner added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { DOMMessage, DOMMessageResponse } from './chromeServices/types';
 
 function App() {
+  const [title, setTitle] = useState<string>('');
+  const [headlines, setHeadlines] = useState<string[]>([]);
+
+  useEffect(() => {
+    chrome.tabs &&
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true,
+        },
+        (tabs) => {
+          chrome.tabs.sendMessage(
+            tabs[0].id || 0,
+            {
+              type: 'GET_DOM',
+            } as DOMMessage,
+            (response: DOMMessageResponse) => {
+              setTitle(response.title);
+              setHeadlines(response.headlines);
+            }
+          );
+        }
+      );
+  }, []);
+
   return (
     <div className="App">
       <h1>SEO Extension built with React!</h1>
@@ -10,20 +36,34 @@ function App() {
         <li className="SEOValidation">
           <div className="SEOValidationField">
             <span className="SEOValidationFieldTitle">Title</span>
-            <span className="SEOValidationFieldStatus Error">
-              90 Characters
+            <span
+              className={`SEOValidationFieldStatus ${
+                title?.length < 30 || title?.length > 65 ? 'Error' : 'Ok'
+              }`}
+            >
+              {title?.length} Characters
             </span>
           </div>
-          <div className="SEOVAlidationFieldValue">The title of the page</div>
+          <div className="SEOVAlidationFieldValue">{title ?? ''}</div>
         </li>
 
         <li className="SEOValidation">
           <div className="SEOValidationField">
             <span className="SEOValidationFieldTitle">Main Heading</span>
-            <span className="SEOValidationFieldStatus Ok">1</span>
+            <span
+              className={`SEOValidationFieldStatus ${
+                headlines?.length !== 1 ? 'Error' : 'Ok'
+              }`}
+            >
+              {headlines?.length}
+            </span>
           </div>
           <div className="SEOVAlidationFieldValue">
-            The main headline of the page (H1)
+            <ul>
+              {headlines?.map((headline, index) => (
+                <li key={index}>{headline}</li>
+              ))}
+            </ul>
           </div>
         </li>
       </ul>
